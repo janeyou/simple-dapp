fa266c8c2f08ac595388edccfc8e2f3a85210e5f
Jane You <jane2745@gmail.com>
2022-01-08 14:29:25 -0800

    ropsten test net with value input

--- dapp.js
+++ dapp.js
@@ -1,3 +1,34 @@
+const ssAddress = "0xf1482a219afbf8f4b3e015c9992f9eade9927c2e";
+
+const ssABI = [
+  {
+    inputs: [],
+    name: "retrieve",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "num",
+        type: "uint256",
+      },
+    ],
+    name: "store",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
 // 1. detect Metamask
 window.addEventListener("load", function () {
   if (typeof this.window.ethereum !== "undefined") {
@@ -18,4 +49,21 @@ mmEnable.onclick = async () => {
   const mmCurrentAccount = document.getElementById("mm-current-account");
 
   mmCurrentAccount.innerHTML = "Current account: " + ethereum.selectedAddress;
+};
+
+const ssSubmit = document.getElementById("ss-input-button");
+
+ssSubmit.onclick = async () => {
+  const ssValue = document.getElementById("ss-input-box").value;
+  console.log(ssValue);
+
+  var web3 = new Web3(window.ethereum);
+
+  const simpleStorage = new web3.eth.Contract(ssABI, ssAddress);
+
+  simpleStorage.setProvider(window.ethereum);
+
+  await simpleStorage.methods
+    .store(ssValue)
+    .send({ from: ethereum.selectedAddress });
 };
